9856d4f5cddbcb1fc438bc70106b67d334814d8c
Alex <alex_smr_91@mail.ru>
2023-03-16 23:07:40 +0400

    dz8_dikov_alex

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from blog.config import BaseConfig, DevConfig, TestingConfig
 from blog.article.views import article
-from blog.user.views import user
+from blog.user.views import users
 from blog.models import db
 from blog.auth import auth
 from blog.auth import login_manager
 from flask_migrate import Migrate
 from blog.authors.views import authors_app
+from blog.admin import admin
+
 
 
 def create_app(config_class=DevConfig) -> Flask:
@@ -15,12 +17,13 @@ def create_app(config_class=DevConfig) -> Flask:
     db.init_app(app)
     migrate = Migrate(app, db, compare_type=True)
     login_manager.init_app(app)
+    admin.init_app(app)
     register_blueprints(app)
     return app
 
 
 def register_blueprints(app: Flask):
     app.register_blueprint(article,  url_prefix='/article')
-    app.register_blueprint(user, url_prefix='/user')
+    app.register_blueprint(users, url_prefix='/user')
     app.register_blueprint(authors_app, url_prefix="/authors")
     app.register_blueprint(auth)
--- blog/models/article.py
+++ blog/models/article.py
@@ -19,3 +19,7 @@ class Article(db.Model):
         back_populates="articles",
     )
 
+    def __str__(self):
+        return self.title
+
+

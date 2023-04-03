a2114319d705856060ab59a46efcab1f72db835e
Dikov Alex <alex_smr_91@mail.ru>
2023-04-03 20:51:39 +0400

    dz11_dikov_alex

--- blog/views/first_start.py
+++ blog/views/first_start.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, render_template
+import os
 
-from wsgi import init_db, create_admin, create_tags
 
 first_start = Blueprint("first_start", __name__)
 
 
 @first_start.route("/", endpoint="firs_start")
-def first_start():
-    init_db()
-    create_admin()
-    create_tags()
+def first_start_view():
+    os.system("flask init_db | flask create-admin | flask create-tags")
     return render_template("article/article_list.html")
